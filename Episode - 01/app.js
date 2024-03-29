2c875cc6b6494e8b64e99c3bae41d15184ba40e9
archit1210 <architlokhande72@gmail.com>
2024-03-29 23:09:58 +0530

    episode 1 updated

--- Episode - 01/app.js
+++ Episode - 01/app.js
@@ -18,7 +18,7 @@ const parent = React.createElement(
     React.createElement(
         "div",
         { id: "child" },
-        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]));
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2  tag")]));
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
